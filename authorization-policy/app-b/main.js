9b682e3c877946c1fbb7e8a7f4ee02110ba5495c
Álvaro Oliveira <algab.oliveira@gmail.com>
2023-12-12 18:38:01 -0300

    add folder authorization-policy

---
+++ authorization-policy/app-b/main.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+const port = 3001;
+
+const APP_A = process.env.APP_A || "http://localhost:3000";
+const APP_C = process.env.APP_C || "http://localhost:3002";
+
+app.use(cors());
+
+app.get("/b", (_, res) => {
+  res.status(200).json({ message: "Application B" }).end();
+});
+
+app.get("/b/a", async (_, res) => {
+  try {
+    await axios(`${APP_A}/a`);
+    res.status(200).json({ message: "Application B -> A" }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+app.get("/b/c", async (_, res) => {
+  try {
+    await axios(`${APP_C}/c`);
+    res.status(200).json({ message: "Application B -> C" }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
---
+++ authorization-policy/app-c/main.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+const port = 3002;
+
+const APP_A = process.env.APP_A || "http://localhost:3000";
+const APP_B = process.env.APP_B || "http://localhost:3001";
+
+app.use(cors());
+
+app.get("/c", (_, res) => {
+  res.status(200).json({ message: "Application C" }).end();
+});
+
+app.get("/c/a", async (_, res) => {
+  try {
+    await axios(`${APP_A}/a`);
+    res.status(200).json({ message: "Application C -> A" }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+app.get("/c/b", async (_, res) => {
+  try {
+    await axios(`${APP_B}/b`);
+    res.status(200).json({ message: "Application C -> B" }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
