c48b7c043f7ce38e86fd6b18378a17595279e393
Álvaro Oliveira <algab.oliveira@gmail.com>
2023-12-14 18:55:50 -0300

    adding tls to ingress

--- authorization-policy/front/script.js
+++ authorization-policy/front/script.js
@@ -1,80 +1,98 @@
 function requestA() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-a/a").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP A")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-a/a")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP A");
+    });
 }
 
 function requestAB() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-a/a/b").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP A with APP B")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-a/a/b")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP A with APP B");
+    });
 }
 
 function requestAC() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-a/a/c").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP A with APP C")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-a/a/c")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP A with APP C");
+    });
 }
 
 function requestB() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-b/b").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP B")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-b/b")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP B");
+    });
 }
 
 function requestBA() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-b/b/a").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP B with APP A")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-b/b/a")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP B with APP A");
+    });
 }
 
 function requestBC() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-b/b/c").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP B with APP C")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-b/b/c")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP B with APP C");
+    });
 }
 
 function requestC() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-c/c").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP C")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-c/c")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP C");
+    });
 }
 
 function requestCA() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-c/c/a").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP C with APP A")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-c/c/a")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP C with APP A");
+    });
 }
 
 function requestCB() {
-  fetch("http://poc-istio-authorization-policy.localdomain/app-c/c/b").then(async (response) => {
-    const data = await response.json();
-    alert(data.message);
-  }).catch(() => {
-    alert("Error communicating with APP C with APP B")
-  });
+  fetch("https://authorization-policy.poc-istio.localdomain/app-c/c/b")
+    .then(async (response) => {
+      const data = await response.json();
+      alert(data.message);
+    })
+    .catch(() => {
+      alert("Error communicating with APP C with APP B");
+    });
 }
